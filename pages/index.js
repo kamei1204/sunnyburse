c2d166b9a9ab2f7488e9ede9c5259e8b2c2c54a0
kameiryousuke <kamei.r@i.softbank.jp>
2023-02-21 00:52:38 +0900

    変更

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,25 @@
-// import { Footer, Navbar } from "../components";
-// import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from "../sections";
+import { Footer, Navbar } from "../components";
+import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from "../sections";
 
-// const Page = () => (
-// <div className=" overflow-hidden lg:p-20">
-//     <Navbar />
-//     <Hero />
-//     <div className="relative">
-//         <About />
-//         <div className="gradient-03 z-0" />
-//         <Explore />
-//     </div>
-//     <div className="relative">
-//         <GetStarted />
-//         <div className="gradient-03 z-0" />
-//         <WhatsNew />
-//     </div>
-//     {/* <World /> */}
-//     <Insights />
-//     <Feedback />
-//     <Footer />
-// </div>
-// );
+const Page = () => (
+<div className=" overflow-hidden lg:p-20">
+    <Navbar />
+    <Hero />
+    <div className="relative">
+        <About />
+        <div className="gradient-03 z-0" />
+        <Explore />
+    </div>
+    <div className="relative">
+        <GetStarted />
+        <div className="gradient-03 z-0" />
+        <WhatsNew />
+    </div>
+    {/* <World /> */}
+    <Insights />
+    <Feedback />
+    <Footer />
+</div>
+);
 
-// export default Page;
+export default Page;
