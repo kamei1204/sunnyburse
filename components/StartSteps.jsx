396c97b78ac973f51cdc9c1f0fc04c2ba068dd57
kameiryousuke <kamei.r@i.softbank.jp>
2023-02-21 09:47:33 +0900

    eslint変更

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -1,12 +1,16 @@
-import styles from "../styles"
+"use client";
+
+import styles from "../styles";
 
 const StartSteps = ({ number, text }) => (
-  <div className={`${styles.flexCenter} flex-row`}>
-    <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5b]`}>
-      <p className="font-bold text-white text-[20px]">0{number}</p>
+  <>
+    <div className={`${styles.flexCenter} flex-row`}>
+      <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5b]`}>
+        <p className="font-bold text-white text-[20px]">0{number}</p>
+      </div>
+      <p className="flex-1 ml-[30px] font-normal text-[#b0b0b0] text-[18px] leading-[32px]">{text}</p>
     </div>
-    <p className="flex-1 ml-[30px] font-normal text-[#b0b0b0] text-[18px] leading-[32px]">{text}</p>
-  </div>
+  </>
 );
 
 export default StartSteps;
--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import styles from "../styles";
 import { motion } from "framer-motion";
